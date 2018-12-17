6928e928efcdf2f833f5e8159864478a5d0d4ff6
DenisBashkirov <era-digital@yandex.ru>
2018-12-17 22:05:40 +0300

    Update delivery.blade.php

---
+++ resources/views/frontend/pages/home/delivery.blade.php
@@ -0,0 +1,9 @@
+<div class="delivery section">
+    <div class="delivery__container section__container">
+
+        <div class="section__header">
+            <p class="section__header-title">Доставка</p>
+        </div>
+
+    </div>
+</div>
