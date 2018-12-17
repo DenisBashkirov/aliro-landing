c9f65e7b2a33ba535193c269992ef3df9ce1996d
DenisBashkirov <era-digital@yandex.ru>
2018-12-17 22:31:46 +0300

    commit

--- resources/views/frontend/pages/home/delivery.blade.php
+++ resources/views/frontend/pages/home/delivery.blade.php
@@ -1,5 +1,5 @@
 <div class="delivery section">
-    <div class="delivery__container section__container">
+    <div class="delivery__container section__container container">
 
         <div class="section__header">
             <p class="section__header-title">Доставка</p>
